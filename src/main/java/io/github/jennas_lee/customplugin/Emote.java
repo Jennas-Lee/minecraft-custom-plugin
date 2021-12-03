fbbd422a27efe2dddb9bdc81e5a2e9658106662f
Jennas-Lee <leezeunjun@gmail.com>
2021-12-03 11:58:51 +0900

    Add to spit emote

--- src/main/java/io/github/jennas_lee/customplugin/Emote.java
+++ src/main/java/io/github/jennas_lee/customplugin/Emote.java
@@ -2,34 +2,112 @@ package io.github.jennas_lee.customplugin;
 
 import org.bukkit.Location;
 import org.bukkit.Particle;
+import org.bukkit.Sound;
 import org.bukkit.World;
 import org.bukkit.entity.Player;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.TreeMap;
 
 public class Emote {
-    private TreeMap emotes = new TreeMap<String, Location>(String.CASE_INSENSITIVE_ORDER);
+    private Player player = null;
 
     Emote(Player player) {
-        register("angry", "화남", player.spawnParticle(
-                Particle.VILLAGER_ANGRY,
-                player.getLocation().getX(),
-                player.getLocation().getY() + 2.0,
-                player.getLocation().getZ(),
-                0,
-                0.0,
-                0.0,
-                0.0,
-                null
-        ));
+        this.player = player;
     }
 
-    private void register(String name, String subname, Location emote) {
-        emotes[name] = emote;
-        emotes[subname] = emote;
-    }
-
-    Location emoteBy(String name) {
-        return emotes[name];
+    void emoteBy(String message) {
+        if (message.equals("angry") || message.equals("화남")) {
+            player.getWorld().spawnParticle(
+                    Particle.VILLAGER_ANGRY,
+                    player.getLocation().getX(),
+                    player.getLocation().getY() + 2.0,
+                    player.getLocation().getZ(),
+                    0,
+                    0.0,
+                    0.0,
+                    0.0,
+                    0.0,
+                    null,
+                    true
+            );
+        } else if(message.equals("heart") || message.equals("하트")) {
+            player.getWorld().spawnParticle(
+                    Particle.HEART,
+                    player.getLocation().getX(),
+                    player.getLocation().getY() + 2.0,
+                    player.getLocation().getZ(),
+                    0,
+                    0.0,
+                    0.0,
+                    0.0,
+                    0.0,
+                    null,
+                    true
+            );
+        } else if(message.equals("ㅗ") || message.equals("망할")) {
+            player.getWorld().spawnParticle(
+                    Particle.HEART,
+                    player.getLocation().getX(),
+                    player.getLocation().getY() + 2.0,
+                    player.getLocation().getZ(),
+                    0,
+                    0.0,
+                    0.0,
+                    0.0,
+                    0.0,
+                    null,
+                    true
+            );
+        } else if(message.equals("damage") || message.equals("상처")) {
+            player.getWorld().spawnParticle(
+                    Particle.DAMAGE_INDICATOR,
+                    player.getLocation().getX(),
+                    player.getLocation().getY() + 2.0,
+                    player.getLocation().getZ(),
+                    0,
+                    0.0,
+                    0.0,
+                    0.0,
+                    0.0,
+                    null,
+                    true
+            );
+        } else if(message.equals("critical") || message.equals("많은상처")) {
+            player.getWorld().spawnParticle(
+                    Particle.DAMAGE_INDICATOR,
+                    player.getLocation().getX(),
+                    player.getLocation().getY() + 2.0,
+                    player.getLocation().getZ(),
+                    16,
+                    0.5,
+                    0.5,
+                    0.5,
+                    0.0,
+                    null,
+                    true
+            );
+        } else if(message.equals("spit") || message.equals("퉤")) {
+            player.getWorld().spawnParticle(
+                    Particle.SPIT,
+                    player.getLocation().getX(),
+                    player.getLocation().getY() + 1.62,
+                    player.getLocation().getZ(),
+                    0,
+                    player.getLocation().getDirection().getX(),
+                    player.getLocation().getDirection().getX(),
+                    player.getLocation().getDirection().getX(),
+                    1.0,
+                    null,
+                    true
+            );
+            player.getWorld().playSound(
+                    player.getLocation(),
+                    Sound.ENTITY_LLAMA_SPIT,
+                    1.0F,
+                    1.0F
+            );
+        }
     }
 }
